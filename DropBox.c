48a81fc2ef59c1f0ba31c27e92ac9320f42671ba
Mihail-Makei <mihasiknet@phystech.edu>
2020-10-05 04:00:06 +0300

    Hard link

--- DropBox.c
+++ DropBox.c
@@ -1,417 +1,295 @@
-#include <fltkernel.h>
+/*++
+
+Module Name:
+
+    HardLink.c
+
+Abstract:
+
+    This is the main module of the HardLink miniFilter driver.
+
+Environment:
+
+    Kernel mode
+
+--*/
+
+#include <fltKernel.h>
 #include <dontuse.h>
-#include <wdm.h>
 
-#define MAX_FILE_NAME_SIZE 500
+#pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
 
-PFLT_FILTER FilterHandle = NULL;
 
-//----------------------------------------------
-//---------------Declarations-------------------
-//----------------------------------------------
+PFLT_FILTER gFilterHandle;
+ULONG_PTR OperationStatusCtx = 1;
+#define MAX_FILE_NAME_SIZE 1000
 
-//
-// DriverEntry routine
-//
+
+
+/*************************************************************************
+    Prototypes
+*************************************************************************/
+
+EXTERN_C_START
 
 DRIVER_INITIALIZE DriverEntry;
-NTSTATUS DriverEntry(
-	PDRIVER_OBJECT	DriverObject,
-	PUNICODE_STRING	RegistryPath
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
 );
 
-//
-// Driver unload routine
-//
 
-NTSTATUS DriverUnload(
-	FLT_FILTER_UNLOAD_FLAGS Flags
+NTSTATUS
+HardLinkUnload(
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
 );
 
-//
-// IRP_MJ_CREATE preoperation callback
-//
+NTSTATUS
+HardLinkInstanceQueryTeardown(
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ FLT_INSTANCE_QUERY_TEARDOWN_FLAGS Flags
+);
 
 FLT_PREOP_CALLBACK_STATUS PreCreate(
-	PFLT_CALLBACK_DATA		Data,
-	PCFLT_RELATED_OBJECTS	RelatedObjects,
-	PVOID*					CompletionContext
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+);
+
+VOID HardLinkOperationStatusCallback(
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ PFLT_IO_PARAMETER_BLOCK ParameterSnapshot,
+    _In_ NTSTATUS OperationStatus,
+    _In_ PVOID RequesterContext
+);
+
+FLT_POSTOP_CALLBACK_STATUS PostCreate(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags
 );
 
 
-FLT_PREOP_CALLBACK_STATUS PreRead(
-	PFLT_CALLBACK_DATA		Data,
-	PCFLT_RELATED_OBJECTS	RelatedObjects,
-	PVOID*					CompletionContext
-);
+EXTERN_C_END
 
 //
-// IRP_MJ_WRITE preoperation callback
+//  Assign text sections for each routine.
 //
-
-FLT_PREOP_CALLBACK_STATUS PreWrite(
-	PFLT_CALLBACK_DATA		Data,
-	PCFLT_RELATED_OBJECTS	RelatedObjects,
-	PVOID*					CompletionContext
-);
-
-//
-// Useless routine printing file name
-//
-
-NTSTATUS PrintFileName(
-	PFLT_CALLBACK_DATA Data
-);
-
-//
-// Filter operation registration data
-//
-
-
-CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
-	{IRP_MJ_CREATE,
-	0,
-	PreCreate,
-	NULL},
-
-	{IRP_MJ_WRITE,
-	0,
-	PreWrite,
-	NULL},
-	
-	{IRP_MJ_OPERATION_END}
-};
-
-
-//
-// Filter registration data
-//
-
-CONST FLT_REGISTRATION Registration = {
-	sizeof(FLT_REGISTRATION),
-	FLT_REGISTRATION_VERSION,
-	0,
-
-	NULL,
-	Callbacks,
-
-	DriverUnload,
-
-	NULL,
-	NULL,
-	NULL,
-
-	NULL,
-	NULL,
-	NULL,
-	NULL
-};
 
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text(INIT, DriverEntry)
-#pragma alloc_text(PAGE, DriverUnload)
+#pragma alloc_text(PAGE, HardLinkUnload)
+
 #endif
 
 //
-// DriverEntry routine
+//  operation registration
 //
 
-NTSTATUS DriverEntry(
-	PDRIVER_OBJECT	DriverObject,
-	PUNICODE_STRING	RegistryPath
+CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
+
+    { IRP_MJ_CREATE,
+    0,
+    PreCreate,
+    PostCreate},
+
+    { IRP_MJ_OPERATION_END }
+};
+
+//
+//  This defines what we want to filter with FltMgr
+//
+
+CONST FLT_REGISTRATION FilterRegistration = {
+
+    sizeof(FLT_REGISTRATION),         //  Size
+    FLT_REGISTRATION_VERSION,           //  Version
+    0,                                  //  Flags
+
+    NULL,                               //  Context
+    Callbacks,                          //  Operation callbacks
+
+    HardLinkUnload,                     //  MiniFilterUnload
+
+    NULL,                               //  InstanceSetup
+    NULL,                               //  InstanceQueryTeardown
+    NULL,                               //  InstanceTeardownStart
+    NULL,                               //  InstanceTeardownComplete
+
+    NULL,                               //  GenerateFileName
+    NULL,                               //  GenerateDestinationFileName
+    NULL                                //  NormalizeNameComponent
+
+};
+
+
+
+
+
+
+/*************************************************************************
+    MiniFilter initialization and unload routines.
+*************************************************************************/
+
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
 ) {
-	UNREFERENCED_PARAMETER(RegistryPath);
+    NTSTATUS status = STATUS_SUCCESS;
 
-	//
-	// Registating filter
-	//
+    UNREFERENCED_PARAMETER(RegistryPath);
 
-	NTSTATUS status = FltRegisterFilter(
-		DriverObject, 
-		&Registration, 
-		&FilterHandle
-	);
+    DbgPrint("Loaded!\n");
 
-	if (!NT_SUCCESS(status)) {
-		DbgPrint("Filter registration failed!\r\n");
+    status = FltRegisterFilter(DriverObject,
+        &FilterRegistration,
+        &gFilterHandle);
 
-		return status;
-	}
+    FLT_ASSERT(NT_SUCCESS(status));
 
-	DbgPrint("Registered!\r\n");
+    if (NT_SUCCESS(status)) {
+        status = FltStartFiltering(gFilterHandle);
 
-	status = FltStartFiltering(FilterHandle);
+        if (!NT_SUCCESS(status))
+            FltUnregisterFilter(gFilterHandle);
 
-	if (!NT_SUCCESS(status)) {
-		DbgPrint("Filtering not started!\r\n");
+    }
 
-		FltUnregisterFilter(FilterHandle);
-
-		return status;
-	}
-
-	DbgPrint("Loaded!\r\n");
-
-	return STATUS_SUCCESS;
+    return status;
 }
 
-//
-// Driver unload function
-//
-
-NTSTATUS DriverUnload(
-	FLT_FILTER_UNLOAD_FLAGS Flags
+NTSTATUS
+HardLinkUnload(
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
 ) {
-	UNREFERENCED_PARAMETER(Flags);
+    UNREFERENCED_PARAMETER(Flags);
 
-	PAGED_CODE();
-	
-	FltUnregisterFilter(FilterHandle);
+    PAGED_CODE();
 
-	DbgPrint("Unloaded!\r\n");
+    KdPrint(("Unloaded!\n"));
 
-	return STATUS_SUCCESS;
+    FltUnregisterFilter(gFilterHandle);
+
+    return STATUS_SUCCESS;
 }
 
-//
-// Preoperation callback
-//
 
+/*************************************************************************
+    MiniFilter callback routines.
+*************************************************************************/
 FLT_PREOP_CALLBACK_STATUS PreCreate(
-	PFLT_CALLBACK_DATA		Data,
-	PCFLT_RELATED_OBJECTS	RelatedObjects,
-	PVOID*					CompletionContext
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext
 ) {
-	NTSTATUS status = STATUS_SUCCESS;
-	HANDLE			   FileHandle = NULL;
-	IO_STATUS_BLOCK    IoStatusBlock = {};
-	PFLT_FILE_NAME_INFORMATION FileNameInformation = NULL;
-	WCHAR			   FileName[MAX_FILE_NAME_SIZE] = { 0 };
-	UNICODE_STRING     SyncName;
-	OBJECT_ATTRIBUTES  ObjectAttributes;
+    UNREFERENCED_PARAMETER(Data);
+    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(CompletionContext);
 
-	UNREFERENCED_PARAMETER(Data);
-	UNREFERENCED_PARAMETER(RelatedObjects);
-	UNREFERENCED_PARAMETER(CompletionContext);
-	
+    DbgPrint("Create callback invoked!\r\n");
 
-	//
-	// Getting file name information
-	//
-
-	status = FltGetFileNameInformation(
-		Data,
-		FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
-		&FileNameInformation
-	);
-
-	if (NT_SUCCESS(status)) {
-		status = FltParseFileNameInformation(FileNameInformation);
-
-		if (NT_SUCCESS(status) && FileNameInformation->Name.MaximumLength < MAX_FILE_NAME_SIZE) {
-			//
-			// Copying file name to memory
-			//
-			RtlCopyMemory(FileName, FileNameInformation->Name.Buffer, FileNameInformation->Name.MaximumLength);
-			
-			if (wcsstr(FileName, L"\\Device\\HarddiskVolume2\\FolderOne\\") != NULL) {
-				DbgPrint("Ñreate to be done!\n");
-				WCHAR Name[MAX_FILE_NAME_SIZE] = L"\\DosDevices\\C:\\FolderTwo\\";
-				wcscpy(&Name[25], &FileName[34]);
-
-				DbgPrint("NAME: %ws\n", Name);
-				RtlInitUnicodeString(&SyncName, Name);
-
-				InitializeObjectAttributes(
-					&ObjectAttributes, 
-					&SyncName,
-					OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE,
-					NULL, 
-					NULL
-				);
-
-				
-
-				//
-				// Check if IRQL = 0
-				//
-
-				if (KeGetCurrentIrql() != PASSIVE_LEVEL)
-					return STATUS_INVALID_DEVICE_STATE;
-				//
-				// Creating file if it does not exist in Folder 2
-				//
-
-				status = ZwCreateFile(
-					&FileHandle,
-					GENERIC_WRITE,
-					&ObjectAttributes, 
-					&IoStatusBlock, 
-					NULL,
-					FILE_ATTRIBUTE_NORMAL,
-					0,
-					FILE_CREATE,
-					FILE_SYNCHRONOUS_IO_NONALERT,
-					NULL, 
-					0
-				);
-
-				if (NT_SUCCESS(status) && NT_SUCCESS(IoStatusBlock.Information))
-					ZwClose(FileHandle);
-			}
-		}
-
-		FltReleaseFileNameInformation(FileNameInformation);
-	}
-
-	return FLT_PREOP_SUCCESS_NO_CALLBACK;
+    return FLT_PREOP_SUCCESS_WITH_CALLBACK;
 }
 
 
 
-
-
-FLT_PREOP_CALLBACK_STATUS PreWrite(
-	PFLT_CALLBACK_DATA		Data,
-	PCFLT_RELATED_OBJECTS	RelatedObjects,
-	PVOID*					CompletionContext
+FLT_POSTOP_CALLBACK_STATUS PostCreate(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags
 ) {
-	PFLT_FILE_NAME_INFORMATION FileNameInformation = NULL;
-	WCHAR FileName[MAX_FILE_NAME_SIZE] = { 0 };
-	UNICODE_STRING     uniName = {};
-	OBJECT_ATTRIBUTES  objAttr = {};
-	HANDLE			   handle = 0;
-	IO_STATUS_BLOCK    ioStatusBlock;
-	WCHAR Name[MAX_FILE_NAME_SIZE] = L"\\DosDevices\\C:\\FolderTwo\\";
+    NTSTATUS status = STATUS_SUCCESS;
+    IO_STATUS_BLOCK IoStatusBlock;
+    HANDLE FileHandle;
+    PFLT_FILE_NAME_INFORMATION FileNameInformation;
+    WCHAR Name[MAX_FILE_NAME_SIZE] = L"\\DosDevices\\C:\\FolderTwo\\";
 
-	UNREFERENCED_PARAMETER(RelatedObjects);
-	UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(Flags);
 
-	NTSTATUS status = FltGetFileNameInformation(
-		Data,
-		FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
-		&FileNameInformation
-	);
+    status = FltGetFileNameInformation(
+        Data, 
+        FLT_FILE_NAME_NORMALIZED,
+        &FileNameInformation
+    );
 
-	if (NT_SUCCESS(status)) {
-		status = FltParseFileNameInformation(FileNameInformation);
+    if (!NT_SUCCESS(status)) {
+        goto PostCreateExit;
+    }
 
-		if (NT_SUCCESS(status) && FileNameInformation->Name.MaximumLength < MAX_FILE_NAME_SIZE) {
-			RtlCopyMemory(FileName, FileNameInformation->Name.Buffer, FileNameInformation->Name.MaximumLength);
+    status = FltParseFileNameInformation(FileNameInformation);
 
-			if (wcsstr(FileName, L"\\Device\\HarddiskVolume2\\FolderOne\\") != NULL)
-				DbgPrint("Write to be done!\n");
+    if (!NT_SUCCESS(status)) {
+        goto PostCreateRelease;
+    }
 
-			wcscpy(&Name[25], &FileName[34]);
+    PWCHAR FileName = ExAllocatePool(NonPagedPool, FileNameInformation->Name.MaximumLength * sizeof(WCHAR));
+    RtlCopyMemory(FileName, FileNameInformation->Name.Buffer, FileNameInformation->Name.MaximumLength);
 
-			DbgPrint("NAME: %ws\n", Name);
-			
-			RtlInitUnicodeString(&uniName, Name);
-			
-			InitializeObjectAttributes(
-				&objAttr, &uniName,
-				OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE,
-				NULL, 
-				NULL
-			);
+    if (wcsstr(FileName, L"\\Device\\HarddiskVolume2\\FolderOne\\") == NULL) {
+        ExFreePool(FileName);
 
-			//
-			// Check IRQL
-			//
-
-			if (KeGetCurrentIrql() != PASSIVE_LEVEL)
-				return STATUS_INVALID_DEVICE_STATE;
-
-			status = ZwCreateFile(&handle,
-				GENERIC_WRITE,
-				&objAttr, &ioStatusBlock, NULL,
-				FILE_ATTRIBUTE_NORMAL,
-				0,
-				FILE_OPEN_IF,
-				FILE_SYNCHRONOUS_IO_NONALERT,
-				NULL, 0);
-
-			
-			
-
-			if (NT_SUCCESS(status)) {
-				LARGE_INTEGER Offset = Data->Iopb->Parameters.Write.ByteOffset;
-				ULONG BufferLength = Data->Iopb->Parameters.Write.Length;
-				PVOID Buffer = MmAllocateNonCachedMemory(BufferLength);
-				
-				//
-				// Copy data to buffer
-				//
-
-				RtlCopyMemory(
-					Buffer, 
-					Data->Iopb->Parameters.Write.WriteBuffer, 
-					BufferLength
-				);
-
-				//
-				// Write to file
-				//
-
-				status = ZwWriteFile(
-					handle, 
-					0, 
-					NULL, 
-					NULL, 
-					&ioStatusBlock, 
-					Buffer, 
-					BufferLength, 
-					&Offset, 
-					NULL
-				);
-
-				if (NT_SUCCESS(status))
-					DbgPrint("WRITTEN!\n");
-
-				//
-				// Free buffer we do not need anymore
-				//
-
-				MmFreeNonCachedMemory(
-					Buffer, 
-					BufferLength
-				);
-
-				ZwClose(handle);
-			}
-		
-		}
-
-		FltReleaseFileNameInformation(FileNameInformation);
-	}
-
-	return FLT_PREOP_SUCCESS_NO_CALLBACK;
-}
+        goto PostCreateRelease;
+    }
+    
+    wcscpy(&Name[25], &FileName[34]);
 
 
-NTSTATUS PrintFileName(
-	PFLT_CALLBACK_DATA Data
-) {
-	PFLT_FILE_NAME_INFORMATION FileNameInformation = NULL;
-	WCHAR FileName[MAX_FILE_NAME_SIZE] = { 0 };
+    status = ObOpenObjectByPointer(
+        Data->Iopb->TargetFileObject,
+        OBJ_KERNEL_HANDLE,
+        NULL,
+        0,
+        NULL,
+        KernelMode,
+        &FileHandle
+    );
+    DbgPrint("New address: %ws %lu\r\n", Name, FileHandle);
 
-	NTSTATUS status = FltGetFileNameInformation(
-		Data,
-		FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
-		&FileNameInformation
-	);
+    if (!NT_SUCCESS(status)) {
+        DbgPrint("Object not opened\r\n");
 
-	if (NT_SUCCESS(status)) {
-		status = FltParseFileNameInformation(FileNameInformation);
+        ExFreePool(FileName);
 
-		if (NT_SUCCESS(status) && FileNameInformation->Name.MaximumLength < MAX_FILE_NAME_SIZE) {
-			RtlCopyMemory(FileName, FileNameInformation->Name.Buffer, FileNameInformation->Name.MaximumLength);
+        goto PostCreateExit;
+    }
 
-			KdPrint(("%ws\r\n", FileName));
-		}
+    ULONG FileNameLength = wcslen(Name);
 
-		FltReleaseFileNameInformation(FileNameInformation);
-	}
+    FILE_LINK_INFORMATION FileInformation = {
+        TRUE,
+        NULL,
+        Name,
+        FileNameLength
+    };
 
-	return status;
+    status = ZwSetInformationFile(
+        FileHandle,
+        &IoStatusBlock,
+        &FileInformation,
+        sizeof(FILE_LINK_INFORMATION),
+        FileLinkInformation
+    );
+
+    if (!NT_SUCCESS(status))
+        DbgPrint("HARDLINK NOT CREATED!\r\n");
+
+    status = ZwClose(FileHandle);
+
+    if (!NT_SUCCESS(status))
+        DbgPrint("FUCK!\r\n");
+
+PostCreateRelease:
+    FltReleaseFileNameInformation(FileNameInformation);
+
+PostCreateExit:
+
+    DbgPrint("Post create callback invoked!\r\n");
+
+    return FLT_POSTOP_FINISHED_PROCESSING;
 }
